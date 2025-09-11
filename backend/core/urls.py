655ffd57d9ae43e1bec738ef2e71335a90a1e944
Saulo Viana <sauloviana483@gmail.com>
2025-09-11 15:03:30 -0300

    feat: simplejwt lib added and authentication urls configurated

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/auth/register/", RegisterView.as_view(), name="auth_register"),
+    path("api/auth/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
