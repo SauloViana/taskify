f8770093eb01217ba7ee14a0607604cffb5265d0
Saulo Viana <sauloviana483@gmail.com>
2025-09-11 22:56:24 -0300

    feat(tasks): implement Task CRUD API restricted to owner

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -15,13 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from users.views import RegisterView
+from rest_framework.routers import DefaultRouter
+from tasks.views import TaskViewSet
+
+router = DefaultRouter()
+router.register("tasks", TaskViewSet, basename="tasks")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("api/auth/register/", RegisterView.as_view(), name="auth_register"),
     path("api/auth/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/", include(router.urls)),
 ]
