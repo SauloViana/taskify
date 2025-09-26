bafd0989b6b16528e1edd3e699e01cdffffd2425
Saulo Viana <sauloviana483@gmail.com>
2025-09-25 21:20:21 -0300

    feat(frontend): react toast installed and login toast implemented

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './App.css';
 import { Routes, Route, Navigate } from "react-router-dom"
 import Login from './pages/Login';
@@ -7,6 +6,7 @@ function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<><div>Dashboard page!</div></>} />
       <Route path="*" element={<Navigate to="/login" />} />
     </Routes>
   );
